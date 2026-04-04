3e1e57f1dbb049ebf9fe5724bbd47e54754d4288
MatheusPasqualoto <matheusjpasqualoto@gmail.com>
2026-04-04 16:57:43 -0300

    Adição de email e senha e validação da senha

--- BackEnd/empresas/serializers.py
+++ BackEnd/empresas/serializers.py
@@ -16,16 +16,5 @@ class EmpresaSerializer(serializers.ModelSerializer):
 
         
 
-"""id_empresa = models.CharField(primary_key=True)
-    cnpj = BRCNPJField(unique = True, verbose_name="CNPJ")
-    
-    #Verificar se precisa adicionar help_text dentro do parênteses    
-    #Caso tenha: help_text="Digite apenas números ou o formato 00.000.000/0000-00"help_text="Digite apenas números ou o formato 00.000.000/0000-00"
 
-    razao_social = models.CharField(max_length=100, blank=False)    
-    telefone_whatsapp = models.CharField(max_length=20, blank=False)
-    data_cadastro = models.DateField(auto_now_add=True, blank=False)
-    descricao_segmento = models.CharField(max_length=60, blank=True)
-    email = models.CharField(unique=True, max_length=256, blank=False)
-    senha = models.CharField(max_length=20, blank=False)"""
     
