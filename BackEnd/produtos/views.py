532287d0c562509dd68a6b4bc64517e894501798
Matheus Jacintho Pasqualoto <matheusjpasqualoto@gmail.com>
2026-05-04 22:02:28 -0300

    Correções Admin

--- BackEnd/produtos/views.py
+++ BackEnd/produtos/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, IsAdminUser
 from produtos.models import Produto
 from produtos.serializers import ProdutoSerializer
 
 class ProdutoCreateListView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticatedOrReadOnly)
     queryset = Produto.objects.all()
     serializer_class = ProdutoSerializer
 
 class ProdutoRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated, IsAdminUser)
     queryset = Produto.objects.all()
     serializer_class = ProdutoSerializer
-# Create your views here.
+
