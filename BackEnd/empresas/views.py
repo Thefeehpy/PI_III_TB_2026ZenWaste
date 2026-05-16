5116ba74ae1e102f0c38d1f71261b0c5ef91f34b
matheuspasqualoto <matheusjpasqualoto@gmail.com>
2026-05-16 16:45:11 -0300

    Separação das classes de Produtos.views

--- BackEnd/empresas/views.py
+++ BackEnd/empresas/views.py
@@ -4,13 +4,13 @@ from empresas.models import Empresa
 from empresas.serializers import EmpresaSerializer
 
 class EmpresaCreateListView(generics.ListCreateAPIView):
-    permission_classes = (IsAuthenticated, IsAuthenticatedOrReadOnly)    
+    #permission_classes = (IsAuthenticated, IsAuthenticatedOrReadOnly)    
     queryset = Empresa.objects.all()
     serializer_class = EmpresaSerializer
 
 
 class EmpresaRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthenticated, IsAdminUser)
+    #permission_classes = (IsAuthenticated, IsAdminUser)
     queryset = Empresa.objects.all()
     serializer_class = EmpresaSerializer
 
--- BackEnd/produtos/views.py
+++ BackEnd/produtos/views.py
@@ -5,12 +5,30 @@ from produtos.models import Produto
 from produtos.serializers import ProdutoSerializer
 
 class ProdutoCreateListView(generics.ListCreateAPIView):
-    permission_classes = (IsAuthenticatedOrReadOnly)
+    #permission_classes = (IsAuthenticatedOrReadOnly)
     queryset = Produto.objects.all()
     serializer_class = ProdutoSerializer
 
 class ProdutoRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthenticated, IsAdminUser)
+    #permission_classes = (IsAuthenticated, IsAdminUser)
     queryset = Produto.objects.all()
     serializer_class = ProdutoSerializer
 
+class ProdutoCreate(generics.CreateAPIView):
+    queryset = Produto.objects.all()
+    serializer_class = ProdutoSerializer
+
+class ProdutoUpdate(generics.UpdateAPIView):
+    queryset = Produto.objects.all()
+    serializer_class = ProdutoSerializer
+
+class ProdutoDestroy(generics.DestroyAPIView):
+    queryset = Produto.objects.all()
+    serializer_class = ProdutoSerializer
+
+class ProdutoRetrieve(generics.RetrieveAPIView):
+    queryset = Produto.objects.all()
+    serializer_class = ProdutoSerializer
+
+
+    
